cac56fa48de31e4f65aaea506a7da9f73d45bed4
JugareanBogdan <153998959+JugareanBogdan@users.noreply.github.com>
2024-12-13 09:25:59 +0200

    lab 8

---
+++ src/main/java/com/parking/parkinglot/HelloServlet.java
@@ -0,0 +1,36 @@
+package com.parking.parkinglot;
+
+import java.io.*;
+
+import com.parking.parkinglot.entities.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("<div style='border: 1px solid red;'>Laborator<div");
+        out.println("</body></html>");
+
+
+
+    }
+
+    public void destroy() {
+    }
+}
